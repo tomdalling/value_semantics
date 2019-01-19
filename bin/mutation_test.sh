0d716e88bd53d89dd5086b1daec4b7eb7dcb2aaf
Tom Dalling <tom@tomdalling.com>
2019-01-20 01:16:22 +1100

    Update mutant
    Can't go to the next version up, because it causes infinite recursion.

--- bin/mutation_test.sh
+++ bin/mutation_test.sh
@@ -3,4 +3,12 @@ set -ue
 
 PATTERN=${1:-ValueSemantics*}
 
-bundle exec mutant --include lib --require value_semantics --use rspec "$PATTERN"
+bundle exec mutant \
+    --include lib \
+    --require value_semantics \
+    --use rspec "$PATTERN" \
+    # Mutant 0.8.24 introduces new mutations that cause infinite recursion inside
+    # #method_missing. These --ignore-subject lines prevent that from happening
+    #--ignore-subject "ValueSemantics::DSL#method_missing" \
+    #--ignore-subject "ValueSemantics::DSL#respond_to_missing?" \
+    #--ignore-subject "ValueSemantics::DSL#def_attr" \
