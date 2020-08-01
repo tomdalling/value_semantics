da40aa718c0d2bca4f65a520320ccdaa6ca363e5
Tom Dalling <tom@tomdalling.com>
2020-08-01 18:07:59 +1000

    Improve API docs (minor)

--- lib/value_semantics.rb
+++ lib/value_semantics.rb
@@ -307,6 +307,8 @@ module ValueSemantics
   # Contains all the configuration necessary to bake a ValueSemantics module
   #
   # @see ValueSemantics.bake_module
+  # @see ClassMethods#value_semantics
+  # @see DSL.run
   #
   class Recipe
     attr_reader :attributes
